e43044856934b1f42f03a719694a8601d66d6ea6
I'm Your Boy Roy <V1x0r87@gmail.com>
2026-02-27 12:33:39 -0700

    chore: normalize line endings and bump version to 0.2.1

--- src/web_scraper_toolkit/__init__.py
+++ src/web_scraper_toolkit/__init__.py
@@ -11,7 +11,7 @@ Usage:
 
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # Configs (Modular)
 from .browser.config import BrowserConfig
--- src/web_scraper_toolkit/server/config.py
+++ src/web_scraper_toolkit/server/config.py
@@ -12,7 +12,7 @@ from dataclasses import dataclass, asdict
 @dataclass
 class ServerConfig:
     name: str = "Web Scraper Toolkit"
-    version: str = "0.2.0"
+    version: str = "0.2.1"
     port: int = 8000
     host: str = "localhost"
     log_level: str = "INFO"
